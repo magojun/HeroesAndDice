d7e2602126342c76c5c8c039d849dc4682fc21ab
magojun <juanczyrka@gmail.com>
2022-09-06 14:15:27 -0300

    Being able to find at least one combination or to find all combinations.

--- dice_roller.py
+++ dice_roller.py
@@ -1,23 +1,61 @@
 import random
+from copy import copy
+
 
 def roll_die():
     return random.randint(1, 6)
 
-def roll_dice(number_of_dice = 5):
+
+def roll_dice(number_of_dice=5):
     dice_roll = []
-    for n in range(number_of_dice):
+    for i in range(number_of_dice):
         dice_roll.append(roll_die())
-    print(dice_roll)
-    print(combination_detected(dice_roll))
     return dice_roll
 
-def combination_detected(dice_roll):
-    detected = False
-    if [2, 2] in dice_roll:
-        detected = True
-    return detected
+
+def get_pairs_combination():
+    pairs_combination = []
+    one_pairs = [1, 1]
+    two_pairs = [2, 2]
+    three_pairs = [3, 3]
+    four_pairs = [4, 4]
+    five_pairs = [5, 5]
+    six_pairs = [6, 6]
+
+    pairs_combination.append(one_pairs)
+    pairs_combination.append(two_pairs)
+    pairs_combination.append(three_pairs)
+    pairs_combination.append(four_pairs)
+    pairs_combination.append(five_pairs)
+    pairs_combination.append(six_pairs)
+    return pairs_combination
+
+
+def has_at_least_one_combination(dice_roll, dice_combinations):
+    return any([has_combination(dice_roll, combination)
+                for combination in dice_combinations])
+
+
+def has_all_combinations(dice_roll, dice_combinations):
+    return all([has_at_least_one_combination(dice_roll, combination)
+                if any(isinstance(combination_set, list) for combination_set in combination)
+                else has_combination(dice_roll, combination)
+                for combination in dice_combinations])
+
+
+def has_combination(dice_roll, dice_combination):
+    # TODO if it's a list of lists fail -> if any(isinstance(combination, list) for combination in dice_combination): throw error
+    dice_roll = copy(dice_roll)  # Avoid updating the actual dictionary elements.
+    combination_found = []
+    for combination in dice_combination:
+        for die in dice_roll:
+            if combination == die:
+                dice_roll.remove(die)
+                combination_found.append(die)
+                break
+    return True if combination_found == dice_combination else False
+
 
 if __name__ == '__main__':
     for n in range(10):
         roll_dice()
-
--- test/test_dice_roller.py
+++ test/test_dice_roller.py
@@ -1,4 +1,5 @@
 import unittest
+from parameterized import parameterized
 
 import dice_roller
 
@@ -17,6 +18,113 @@ class TestDiceRoller(unittest.TestCase):
     def test_roll_dice_with_no_parameters_returns_five_dice(self):
         dice_roll_result = dice_roller.roll_dice()
         self.assertEqual(len(dice_roll_result), 5)
+
+    def test_has_combination_2_2_returns_true_with_numbers_together_on_the_dice_roll(self):
+        dice_roll = [1, 2, 2, 4, 6]
+        dice_combination = [2, 2]
+        has_combination_result = dice_roller.has_combination(dice_roll, dice_combination)
+        self.assertTrue(has_combination_result)
+        self.assertEqual([1, 2, 2, 4, 6], dice_roll)
+
+    def test_has_combination_2_2_returns_true_with_numbers_apart_on_the_dice_roll(self):
+        dice_roll = [1, 2, 4, 2, 6]
+        dice_combination = [2, 2]
+        has_combination_result = dice_roller.has_combination(dice_roll, dice_combination)
+        self.assertTrue(has_combination_result)
+
+    def test_has_combination_2_2_returns_true_with_three_2_on_the_dice_roll(self):
+        dice_roll = [2, 1, 2, 5, 2]
+        dice_combination = [2, 2]
+        has_combination_result = dice_roller.has_combination(dice_roll, dice_combination)
+        self.assertTrue(has_combination_result)
+
+    def test_has_combination_2_2_returns_false_with_one_2_on_the_dice_roll(self):
+        dice_roll = [1, 2, 4, 4, 6]
+        dice_combination = [2, 2]
+        has_combination_result = dice_roller.has_combination(dice_roll, dice_combination)
+        self.assertFalse(has_combination_result)
+
+    def test_get_pairs_combination_returns_correct_combination(self):
+        pairs_combination_expected = [[1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6]]
+        pairs_combination_result = dice_roller.get_pairs_combination()
+        self.assertEqual(pairs_combination_expected, pairs_combination_result)
+
+    @parameterized.expand([
+        [[1, 2, 3, 1, 6]],
+        [[2, 1, 3, 2, 6]],
+        [[3, 1, 2, 3, 6]],
+        [[4, 1, 2, 4, 6]],
+        [[5, 1, 3, 5, 6]],
+        [[6, 1, 3, 2, 6]]
+    ])
+    def test_has_pairs_combination_returns_true_with_one_pair_on_the_dice_roll(self, dice_roll):
+        pairs_combination = dice_roller.get_pairs_combination()
+        has_a_pair_combination_result = dice_roller.has_at_least_one_combination(dice_roll, pairs_combination)
+        self.assertTrue(has_a_pair_combination_result)
+
+    @parameterized.expand([
+        [[1, 2, 3, 4, 6]],
+        [[2, 3, 4, 5, 6]],
+        [[2, 3, 4, 5, 6]],
+        [[3, 4, 5, 6, 1]],
+        [[4, 5, 6, 1, 2]],
+        [[5, 6, 1, 2, 3]]
+    ])
+    def test_has_pairs_combination_returns_false_with_no_pairs_on_the_dice_roll(self, dice_roll):
+        pairs_combination = dice_roller.get_pairs_combination()
+        has_a_pair_combination_result = dice_roller.has_at_least_one_combination(dice_roll, pairs_combination)
+        self.assertFalse(has_a_pair_combination_result)
+
+    @parameterized.expand([
+        [[1, 2, 2, 1, 6]],
+        [[2, 1, 3, 2, 3]],
+        [[3, 4, 4, 3, 6]],
+        [[4, 6, 2, 4, 6]],
+        [[5, 5, 5, 5, 6]],
+        [[6, 1, 3, 1, 6]]
+    ])
+    def test_has_pairs_combination_returns_true_with_two_pairs_on_the_dice_roll(self, dice_roll):
+        pairs_combination = dice_roller.get_pairs_combination()
+        has_a_pair_combination_result = dice_roller.has_at_least_one_combination(dice_roll, pairs_combination)
+        self.assertTrue(has_a_pair_combination_result)
+
+    @parameterized.expand([
+        [[1, 2, 2, 1, 6]],
+        [[2, 2, 2, 2, 3]],
+        [[3, 4, 4, 2, 2]],
+        [[2, 6, 2, 4, 6]],
+        [[2, 2, 5, 5, 2]],
+        [[6, 2, 2, 6, 6]]
+    ])
+    def test_has_pairs_combination_plus_2_2_returns_true_with_adequate_dice_roll(self, dice_roll):
+        pairs_combination = dice_roller.get_pairs_combination()
+        two_2s = [2, 2]
+        expected_combinations = [two_2s, pairs_combination]
+        has_pairs = dice_roller.has_at_least_one_combination(dice_roll, pairs_combination)
+        has_two_2s = dice_roller.has_combination(dice_roll, two_2s)
+        has_expected_combination = dice_roller.has_all_combinations(dice_roll, expected_combinations)
+        self.assertTrue(has_pairs)
+        self.assertTrue(has_two_2s)
+        self.assertTrue(has_expected_combination)
+
+    @parameterized.expand([
+        [[1, 1, 2, 1, 6]],
+        [[2, 3, 1, 3, 3]],
+        [[3, 4, 4, 6, 2]],
+        [[2, 6, 4, 4, 6]],
+        [[5, 5, 5, 5, 2]],
+        [[6, 1, 2, 6, 6]]
+    ])
+    def test_has_pairs_combination_plus_2_2_returns_false_with_dice_roll_that_does_not_contains_2_2(self, dice_roll):
+        pairs_combination = dice_roller.get_pairs_combination()
+        two_2s = [2, 2]
+        expected_combinations = [two_2s, pairs_combination]
+        has_pairs = dice_roller.has_at_least_one_combination(dice_roll, pairs_combination)
+        has_two_2s = dice_roller.has_combination(dice_roll, two_2s)
+        has_expected_combination = dice_roller.has_all_combinations(dice_roll, expected_combinations)
+        self.assertTrue(has_pairs)
+        self.assertFalse(has_two_2s)
+        self.assertFalse(has_expected_combination)
 
 
 if __name__ == '__main__':
